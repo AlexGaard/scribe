f810ccbd377b69d890c7603567f221860fa3492b
Alexander Gård <alexander.olav.gaard@gmail.com>
2024-10-27 18:30:48 +0100

    Simplify email regex

--- src/main/java/com/github/alexgaard/scribe/util/ValidationUtils.java
+++ src/main/java/com/github/alexgaard/scribe/util/ValidationUtils.java
@@ -4,11 +4,12 @@ import java.util.regex.Pattern;
 
 public class ValidationUtils {
 
-    private final static String emailPatternStr = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-    private final static Pattern emailPattern = Pattern.compile(emailPatternStr);
+    private static final Pattern EMAIL_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
 
     public static boolean isEmailValid(String email) {
-        return emailPattern.matcher(email).matches();
+        return EMAIL_REGEX.matcher(email).matches();
     }
 
     public static boolean isValidPortNumber(int portNumber) {
